8a5f97a55630ac42d1ee392f47eebc650e4b1ac0
Edson Junior <edson.fonseca@techne.com.br>
2018-10-19 18:16:24 +0000

    Inserido Bloco Cliente

---
+++ src/main/webapp/js/blockly/Block.blockly.js
@@ -0,0 +1,12 @@
+window.blockly = window.blockly || {};
+window.blockly.js = window.blockly.js || {};
+window.blockly.js.blockly = window.blockly.js.blockly || {};
+window.blockly.js.blockly.Block = window.blockly.js.blockly.Block || {};
+
+/**
+ * Block
+ */
+window.blockly.js.blockly.Block.Execute = function() {
+
+  window.alert('teste');
+}
